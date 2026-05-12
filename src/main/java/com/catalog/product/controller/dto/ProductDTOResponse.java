425f145aed14a5c12f59ef5b330a845c10868538
Andre <lulesaaso2@gmail.com>
2026-05-11 19:24:35 -0500

    refactor: change price type to BigDecimal in ProductDTOResponse for precision

--- src/main/java/com/catalog/product/controller/dto/ProductDTOResponse.java
+++ src/main/java/com/catalog/product/controller/dto/ProductDTOResponse.java
@@ -1,13 +1,15 @@
 package com.catalog.product.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public record ProductDTOResponse(@NotBlank Long id,
                                  @NotBlank String name,
                                  @NotBlank String color,
-                                 @NotBlank String price,
+                                 @NotNull BigDecimal price,
                                  @NotBlank String description,
                                  @NotBlank String brand,
                                  @NotBlank List<ProductStockDTOResponse> productStock
