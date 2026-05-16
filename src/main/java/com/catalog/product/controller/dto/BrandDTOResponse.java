18492715eddf9b101e46b2294a9b8e103bb581b9
Andre <lulesaaso2@gmail.com>
2026-05-15 19:26:54 -0500

    refactor: change id annotation to NotNull in BrandDTOResponse for precision

--- src/main/java/com/catalog/product/controller/dto/BrandDTOResponse.java
+++ src/main/java/com/catalog/product/controller/dto/BrandDTOResponse.java
@@ -1,7 +1,8 @@
 package com.catalog.product.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record BrandDTOResponse (@NotBlank Long id,
+public record BrandDTOResponse (@NotNull Long id,
                                 @NotBlank String brand){
 }
