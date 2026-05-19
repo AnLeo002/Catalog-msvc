cc95c4c97e6c2febda810b1bf4ed9cccedae9bda
Andre <lulesaaso2@gmail.com>
2026-05-19 17:06:25 -0500

    refactor: remove stock parameter in SizeDTOResponse for precision

--- src/main/java/com/catalog/product/controller/dto/SizeDTOResponse.java
+++ src/main/java/com/catalog/product/controller/dto/SizeDTOResponse.java
@@ -3,6 +3,5 @@ package com.catalog.product.controller.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record SizeDTOResponse(@NotBlank Long id,
-                              @NotBlank String size,
-                              @NotBlank Long stock) {
+                              @NotBlank String size) {
 }
