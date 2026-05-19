ea85e80e4b050e08af56f9338a55c0d8b577c64f
Andre <lulesaaso2@gmail.com>
2026-05-19 16:17:32 -0500

    add BrandServiceImpl with all the logic in the methods

---
+++ src/main/java/com/catalog/product/service/impl/BrandServiceImpl.java
@@ -0,0 +1,84 @@
+package com.catalog.product.service.impl;
+
+import com.catalog.product.controller.dto.BrandDTO;
+import com.catalog.product.controller.dto.BrandDTOResponse;
+import com.catalog.product.persistence.BrandEntity;
+import com.catalog.product.repo.BrandRepo;
+import com.catalog.product.service.BrandService;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+public class BrandServiceImpl implements BrandService {
+    private final BrandRepo repo;
+
+    public BrandServiceImpl(BrandRepo brandRepo) {
+        this.repo = brandRepo;
+    }
+
+    @Override
+    @Transactional
+    public BrandDTOResponse createBrand(BrandDTO brandDTO) {
+        if (repo.findByBrandIgnoreCase(brandDTO.brand()).isPresent()){
+            throw new ResponseStatusException(HttpStatus.CONFLICT,"La marca ya se encuentra en la base de datos");
+        }
+        BrandEntity brand = BrandEntity.builder()
+                .brand(brandDTO.brand())
+                .build();
+        repo.save(brand);
+        return new BrandDTOResponse(brand.getId(), brand.getBrand());
+    }
+
+    @Override
+    public BrandDTOResponse findBrandById(Long id) {
+        BrandEntity brand = repo.findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"La marca no se encuentra en la base de datos"));
+        return new BrandDTOResponse(brand.getId(),brand.getBrand());
+    }
+
+    @Override
+    public BrandDTOResponse findBrandByName(String name) {
+        BrandEntity brand = repo.findByBrandIgnoreCase(name)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"La marca no se encuentra en la base de datos"));
+        return new BrandDTOResponse(brand.getId(),brand.getBrand());
+    }
+
+    @Override
+    public List<BrandDTOResponse> findAll() {
+        List<BrandEntity> brandEntities = repo.findAll();
+        if (brandEntities.isEmpty()) throw new ResponseStatusException(HttpStatus.NO_CONTENT,"No existen marcas registradas");
+        return brandEntities.stream()
+                .map(brand -> new BrandDTOResponse(brand.getId(), brand.getBrand()))
+                .toList();
+    }
+
+    @Override
+    @Transactional
+    public BrandDTOResponse updateBrand(BrandDTO brandDTO, Long id) {
+        BrandEntity brand = repo.findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "La marca que va a actualizar no se encuentra en la base de datos"));
+        brand.setBrand(brandDTO.brand());
+        repo.save(brand);
+        return new BrandDTOResponse(brand.getId(), brand.getBrand());
+    }
+
+    @Override
+    public void deleteBrandById(Long id) {
+        if (!repo.existsById(id)){
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT,"La marca no se encuentra en la base de datos");
+        }
+        try{
+            repo.deleteById(id);
+        }catch (DataIntegrityViolationException e){
+            throw new ResponseStatusException(HttpStatus.CONFLICT,"La marca no puede ser eliminada");
+        }
+
+    }
+}
