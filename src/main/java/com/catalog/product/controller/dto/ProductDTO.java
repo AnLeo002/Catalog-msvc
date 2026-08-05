5e80e637682498df4d69abf77fd2243ab5db632f
Andre <lulesaaso2@gmail.com>
2026-08-04 23:26:38 -0500

    add(productDTOs): add color, type, gender and images parameters

--- src/main/java/com/catalog/product/controller/dto/ProductDTO.java
+++ src/main/java/com/catalog/product/controller/dto/ProductDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Set;
 
 public record ProductDTO(@NotBlank String name,
@@ -11,5 +12,8 @@ public record ProductDTO(@NotBlank String name,
                          @NotNull BigDecimal price,
                          @NotBlank String description,
                          @NotBlank String brand,
-                         Set<ProductStockDTO> productStockDTO) {
+                         @NotBlank String type,
+                         @NotBlank String gender,
+                         Set<ProductCreateStockDTO> stockDTOS,
+                         List<String> images) {
 }
--- src/main/java/com/catalog/product/controller/dto/ProductDTOResponse.java
+++ src/main/java/com/catalog/product/controller/dto/ProductDTOResponse.java
@@ -4,14 +4,19 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Set;
 
 public record ProductDTOResponse(@NotNull Long id,
+                                 @NotBlank String sku,
                                  @NotBlank String name,
                                  @NotBlank String color,
+                                 @NotBlank String type,
+                                 @NotBlank String gender,
                                  @NotNull BigDecimal price,
                                  @NotBlank String description,
                                  @NotBlank String brand,
-                                 Set<ProductStockDTOResponse> productStock
+                                 Set<ProductStockDTOResponse> productStock,
+                                 List<String> images
                                  ) {
 }
