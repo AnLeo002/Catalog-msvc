e98b19c65641848060f50e420b4688ab4ce14d9a
Andre <lulesaaso2@gmail.com>
2026-08-04 23:56:31 -0500

    change(ProductServiceImpl): delete productId and sizeId parameters and add ProductSizeId parameter

--- src/main/java/com/catalog/product/controller/dto/ProductStockDTOResponse.java
+++ src/main/java/com/catalog/product/controller/dto/ProductStockDTOResponse.java
@@ -2,11 +2,10 @@ package com.catalog.product.controller.dto;
 
 import com.catalog.product.persistence.primaryKey.ProductSizeId;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record ProductStockDTOResponse(
         @NotBlank ProductSizeId id,
-        @NotBlank Long productId,
-        @NotBlank Long sizeId,
-        @NotBlank Integer stock
+        @NotNull Integer stock
 ) {
 }
