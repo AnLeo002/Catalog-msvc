e30e00c77f4c04bcb7c77ee627b3c468a564d130
Andre <lulesaaso2@gmail.com>
2026-08-04 23:27:04 -0500

    add(ProductEntity): add color, type, gender and images parameters

--- src/main/java/com/catalog/product/persistence/ProductEntity.java
+++ src/main/java/com/catalog/product/persistence/ProductEntity.java
@@ -2,14 +2,18 @@ package com.catalog.product.persistence;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @Builder
+@DynamicUpdate
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "products")
@@ -17,12 +21,23 @@ public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true, nullable = false, length = 50)
+    private String sku;
     private String name;
-    private String color;
     private BigDecimal price;
     private String description;
     @ManyToOne(targetEntity = BrandEntity.class, fetch = FetchType.LAZY)
     private BrandEntity brand;
     @OneToMany(targetEntity = ProductStockEntity.class, fetch = FetchType.LAZY, mappedBy = "product")
     private Set<ProductStockEntity> productStockEntities;
+    @ManyToOne(targetEntity = TypeEntity.class, fetch = FetchType.LAZY)
+    private TypeEntity type;
+    @ManyToOne(targetEntity = GenderEntity.class, fetch = FetchType.LAZY)
+    private GenderEntity gender;
+    @ManyToOne(targetEntity = ColorEntity.class, fetch = FetchType.LAZY)
+    private ColorEntity color;
+    @ElementCollection
+    @CollectionTable(name = "product_images", joinColumns = @JoinColumn(name = "product_id"))
+    @Column(name = "image_url")
+    private List<String> imageUrls = new ArrayList<>();
 }
