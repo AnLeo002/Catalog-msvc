598f082c7dbfa03bedf38b391f82a28cb30f9095
Andre <lulesaaso2@gmail.com>
2026-08-20 18:29:18 -0500

    feat(category): implement new CategoryRepo

---
+++ src/main/java/com/catalog/category/repo/CategoryRepo.java
@@ -0,0 +1,15 @@
+package com.catalog.category.repo;
+
+import com.catalog.category.persistence.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepo extends JpaRepository<CategoryEntity, Long> {
+    @Query("SELECT c FROM CategoryEntity c WHERE LOWER(c.category) = LOWER(:category)")
+    Optional<CategoryEntity> findByCategoryIgnoreCase(@Param("category") String category);
+}
