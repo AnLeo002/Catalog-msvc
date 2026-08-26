ca2f8b70124937217ce54a589a1e4e0bbf3a6180
Andre <lulesaaso2@gmail.com>
2026-08-26 00:36:00 -0500

    feat(category): delete product and service parameters

--- src/main/java/com/catalog/category/persistence/CategoryEntity.java
+++ src/main/java/com/catalog/category/persistence/CategoryEntity.java
@@ -1,11 +1,8 @@
 package com.catalog.category.persistence;
 
-import com.catalog.product.persistence.ProductEntity;
-import com.catalog.service.persistence.ServiceEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.List;
 
 @Entity
 @Getter
@@ -19,8 +16,4 @@ public class CategoryEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String category;
-    @OneToMany(targetEntity = ProductEntity.class,fetch = FetchType.LAZY,mappedBy = "category")
-    private List<ProductEntity> productEntities;
-    @OneToMany(targetEntity = ServiceEntity.class,fetch = FetchType.LAZY,mappedBy = "category")
-    private List<ServiceEntity> serviceEntities;
 }
